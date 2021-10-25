50d0b7fe79baf462cf8e0400b78b33d4b6f4c5af
ninjavalley <92907149+ninjavalley@users.noreply.github.com>
2021-10-25 19:55:15 +0530

    working on bridge call

--- nodejs/web3testcontract.js
+++ nodejs/web3testcontract.js
@@ -1,44 +1,58 @@
-/*
-const Web3 = require("web3");
-let web3 = new Web3(  
-  new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws/v3/c5147069a6de4315aed6494e1fa53266", 30000)
-);
-
-const myabi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_user","type":"address"},{"name":"_amount","type":"uint256"},{"name":"timeOfRequest","type":"uint256"}],"name":"swapReverted","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"signer","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_user","type":"address"},{"name":"_amount","type":"uint256"},{"name":"timeOfRequest","type":"uint256"}],"name":"mintToken","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"tokenType","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"name":"swapStatus","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"userAddress","type":"address"}],"name":"whitelistUser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"changeSafeguardStatus","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"decrease_allowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"user","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"userAddresses","type":"address[]"}],"name":"whitelistManyUsers","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"changeWhitelistingStatus","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"increase_allowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_user","type":"address"},{"name":"_amount","type":"uint256"},{"name":"timeOfRequest","type":"uint256"}],"name":"swapProcessing","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_signer","type":"address"}],"name":"changeSigner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_user","type":"address"},{"name":"_amount","type":"uint256"},{"name":"timeOfRequest","type":"uint256"}],"name":"swapCompleted","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"frozenAccount","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burnToSwap","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"safeguard","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"networkID","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"whitelistingStatus","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"whitelisted","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"target","type":"address"},{"name":"freeze","type":"bool"}],"name":"freezeAccount","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"name_","type":"string"},{"name":"symbol_","type":"string"},{"name":"decimals_","type":"uint256"},{"name":"totalSupply_","type":"uint256"},{"name":"networkID_","type":"uint256"},{"name":"tokenType_","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"timeOfEvent","type":"uint256"}],"name":"BurnToSwapEv","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"timeOfEvent","type":"uint256"}],"name":"swapCompletedEv","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"target","type":"address"},{"indexed":false,"name":"frozen","type":"bool"}],"name":"FrozenAccounts","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"timeOfEvent","type":"uint256"}],"name":"tokenPaidEv","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"}]
-const instance = new web3.eth.Contract(myabi, "0x705CC275544D3B79243B08ea1DAeF8cA1FFb2F85");
-
-instance.events.Transfer().on('data', (event)=>{
-    console.log(">>>>>>>>");
-    console.log(event);
-
-}).on('error', console.error);
-*/
 require('dotenv').config();
-
 const Web3 = require("web3");
+var Tx = require('ethereumjs-tx').Transaction;
 
-/*
-myContract.methods.myMethod(123).send({from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'})
-.on('transactionHash', function(hash){
-})
-.on('confirmation', function(confirmationNumber, receipt){    
-})
-.on('receipt', function(receipt){    
-    console.log(receipt);
-*/
-
-function company_bridge_send_method(_fromWallet, _toWallet, _amt){
-    const company_bridgeinstance = new web3.eth.Contract(JSON.parse(process.env.COMPANY_BRIDGE_ABI), process.env.COMPANY_BRIDGE_ADDR);
-    
-    company_bridgeinstance.methods.returnCoin(_toWallet,_amt).send({from: _fromWallet})
-    .on('transactionHash', function(hash){
-        console.log("Transaction HASH >>>",hash);
-    })
-    .on('receipt', function(receipt){
-        console.log("Receipt >>>",receipt);
-    })
+async function getGasAmount(_fromWallet, _toWallet, _amt, company_bridgeinstance){        
+    var z = await company_bridgeinstance.methods.returnCoin(_toWallet,_amt).estimateGas({from: _fromWallet});
+    return z;
 }
 
+async function company_bridge_send_method(_fromWallet, _toWallet, _amt){
+    let bridgeweb3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/c5147069a6de4315aed6494e1fa53266"));
+    const company_bridgeinstance = new bridgeweb3.eth.Contract(JSON.parse(process.env.COMPANY_BRIDGE_ABI), process.env.COMPANY_BRIDGE_ADDR);    
+    var mydata = company_bridgeinstance.methods.returnCoin(_toWallet,_amt).encodeABI();    
+    var requiredGas = await company_bridgeinstance.methods.returnCoin(_toWallet,_amt).estimateGas({from: _fromWallet});    
+    //console.log("MYDATA >>>>",mydata);       
+    //console.log(">>>>> REQUIRED GAS <<<<<",requiredGas);
+    try{        
+        bridgeweb3.eth.getTransactionCount(_fromWallet,"pending").then((mynonce)=>{    
+            (async function(){                
+                bridgeweb3.eth.getGasPrice().then(gasPrice=>{                                         
+                    const myrawTx = {   
+                        nonce: web3.utils.toHex(mynonce),                    
+                        gasPrice: web3.utils.toHex(gasPrice),
+                        gasLimit: requiredGas,
+                        to: _toWallet,
+                        from: _fromWallet,
+                        value: 0x0, 
+                        data: mydata                  
+                    };  
+                    //console.log("MY RAW TX >>>>>>",myrawTx);                                            
+                    try{      
+                        var tx = new Tx(myrawTx);
+                        var privateKey = Buffer.from(process.env.BRIDGE_CONTRACT_OWNER_PK.toString(), 'hex');
+                        tx.sign(privateKey);                        
+                        var serializedTx = tx.serialize();
+                        bridgeweb3.eth.sendSignedTransaction('0x'+serializedTx.toString('hex'))
+                        .on('transactionHash',function(mytxhash){
+                            console.log("Bridge tx hash >>>",mytxhash);
+                        })                        
+                        .on('error', myerror =>{
+                            console.log("bridge web3 error >>>", myerror);
+                        })
+                        .on('receipt', receipt =>{
+                            console.log("Receipt  >>>",receipt);
+                        })                        
+                    }catch(e){
+                        console.log({"ERROR":e})                                            
+                    }               
+                });                 
+            })();    
+        });        
+    }catch(e){
+        console.log("<<<< Error >>>>>",e);
+    }
+}
 
 
 var myevents = () =>{              
@@ -51,7 +65,8 @@ var myevents = () =>{
         var _transaction_from = res.from;
         var _transaction_to = res.to;
         var _transaction_val = res.value;
-        company_bridge_send_method(_transaction_to, _transaction_val)
+        console.log(_transaction_from, _transaction_to, _transaction_val);
+        company_bridge_send_method(_transaction_from, _transaction_to, _transaction_val)
     }).on('error', console.error);
 }
 
